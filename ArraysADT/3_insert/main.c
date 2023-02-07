1a4f4ca45eae62cf700ad53d13428285636ca4db
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2023-02-06 22:50:24 -0600

    array ADTs: insert, append, delete dynamic,static

---
+++ ArraysADT/3_insert/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Array
+{
+  int A[10];
+  int size;
+  int length;
+};
+
+void Display(struct Array arr)
+{
+  int i;
+  printf("Elements are \n");
+  for(i=0; i<arr.length;i++){
+    printf("%d ",arr.A[i]);
+  }
+  printf("\n");
+}
+
+void Append(struct Array *arr, int x)
+{
+  if(arr->length < arr->size){
+    arr->A[arr->length++]=x;
+  }
+
+}
+
+void Insert(struct Array *arr, int index, int x)
+{
+  if(index >= 0 && index <=arr->length){
+    for(int i = arr->length; i > index; i--)
+      arr->A[i] = arr->A[i-1];
+    arr->A[index] = x;
+    arr->length++;
+  }
+
+}
+
+int main()
+{
+  struct Array arr={{2,3,4,5,6},10,5};
+
+  Append(&arr,10);
+
+  Display(arr);
+
+  Insert(&arr,0,8);
+
+  Display(arr);
+
+  return 0;
+}
