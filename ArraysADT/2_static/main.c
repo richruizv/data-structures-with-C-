8fbb9b59e0703cd7be5c508f8296695025db5a04
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2023-02-11 10:57:45 -0600

    create set get min sum avg rename folders

--- ArraysADT/2_static/main.c
+++ ArraysADT/2_static/main.c
@@ -8,20 +8,62 @@ struct Array
   int length;
 };
 
-void Display(struct Array arr)
-{
-  int i;
-  printf("Elements are \n");
-  for(i=0; i<arr.length;i++){
-    printf("%d ",arr.A[i]);
-  }
+int Get(struct Array arr,int index){
+  if( index>=0 && index < arr.length)
+    return arr.A[index];
+  return -1;
 }
+
+void Set(struct Array arr,int index, int x){
+  if( index>=0 && index < arr.length)
+    arr.A[index] = x;
+}
+
+int Max(struct Array arr)
+{
+  int max=arr.A[0];
+  int i;
+  for(i=1; i< arr.length; i++)
+  {
+      if(arr.A[i]>max)
+        max=arr.A[i];
+  }
+  return max;
+}
+
+int Min(struct Array arr)
+{
+  int min=arr.A[0];
+  int i;
+  for(i=1; i< arr.length; i++)
+  {
+      if(arr.A[i]<min)
+        min=arr.A[i];
+  }
+  return min;
+}
+
+int Sum(struct Array arr)
+{
+  int s=0;
+  int i;
+  for(i=1; i< arr.length; i++)
+    s +=arr.A[i];
+  return s;
+}
+
+float Avg(struct Array arr)
+{
+  return (float)Sum(arr)/arr.length;
+}
+
 
 int main()
 {
   struct Array arr={{2,3,4,5,6},20,5};
 
-  Display(arr);
+
+  printf("%d\n",Sum(arr));
 
   return 0;
 }
